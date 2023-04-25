2f236997fb26ea8b16f0f0ad16658973ede5da43
Felipe Muchulski <lipemuchulski@gmail.com>
2023-04-25 16:18:25 -0300

    Arquivo que recebe resultado do sql_backup.sql

--- executar_backup.sh
+++ executar_backup.sh
@@ -1,4 +1,4 @@
 cd $DIRETORIO
-  pg_dump -U postgres -w -d postgres -C > /mnt/c/Desenv/Projetos-Estudos/Postgres_Backup/backup-postgres-2023-04-24.sql
-  pg_dump -U postgres -w -d muchulski -C > /mnt/c/Desenv/Projetos-Estudos/Postgres_Backup/backup-muchulski-2023-04-24.sql
+  pg_dump -U postgres -w -d postgres -C > /mnt/c/Desenv/Projetos-Estudos/Postgres_Backup/backup-postgres-2023-04-25.sql
+  pg_dump -U postgres -w -d muchulski -C > /mnt/c/Desenv/Projetos-Estudos/Postgres_Backup/backup-muchulski-2023-04-25.sql
 
